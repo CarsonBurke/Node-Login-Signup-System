5d9843418886c338840ce3f328366a560b4e1b72
CarsonBurke <carsonburke22@gmail.com>
2021-05-06 15:00:05 -0700

    added error handling, database show and insert with node

--- index.js
+++ index.js
@@ -12,19 +12,38 @@ const client = new Client({
     password: 'postgres',
     database: 'nodeProject1',
 })
+try {
+    client.connect()
+    console.log("connected to db")
+} catch (error) {
 
-client.connect()
+    console.log("failed to connect to db")
+}
 
-const text = "INSERT INTO users(col1) VALUES('a') RETURNING *"
-
-client.query(text, (err, res) => {
-    if (err) {
-        console.log(err.stack)
+client.query("INSERT INTO users(col1) VALUES('bob') RETURNING *", (error, result) => {
+    if (error) {
+        console.log(error.stack)
     } else {
-        console.log(res.rows[0])
+        console.log(result.rows[0])
+    }
+})
+
+client.query("SELECT * FROM users", (error, result) => {
+    if (error) {
+        console.log(error.stack)
+    } else {
+
+        let rows = result.rows
+
+        for (let row of rows) {
+
+            console.log(row)
+        }
     }
 })
 
 var server = app.listen(5200)
 
 //https://node-postgres.com/
+
+//https://stackoverflow.com/questions/34351395/how-to-return-postgresql-database-result-by-using-pg-and-node-only
